b30e14f6eb0c6cea3a98eb89f262d5470907fbc5
Chofito <rjroblesq@gmail.com>
2019-02-02 11:50:55 -0600

    Add new WriteLine with Welcome message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al curso de .NET!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
